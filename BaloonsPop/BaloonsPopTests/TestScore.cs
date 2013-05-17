486a9cb218acd536c456e32c5816433b1758784e
Ognyan Petkov <ognyan.petkov@gmail.com>
2013-05-17 14:30:57 +0300

    test changed

--- BaloonsPop/BaloonsPopTests/TestScore.cs
+++ BaloonsPop/BaloonsPopTests/TestScore.cs
@@ -1,51 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BaloonsPopGame;
+using System.Collections.Generic;
 
 namespace BaloonsPopTests
 {
     [TestClass]
     public class TestScore
     {
-       /* [TestMethod]
+        [TestMethod]
         public void Compare14To19Score()
         {
-            Score pesho = new Score(14, "Pesho");
-            Score gosho = new Score(19, "Gosho");
-            int actualResult = pesho.CompareTo(gosho);
+            Score players = new Score();
+            players.AddPlayer("Gosho", 14);
+            players.AddPlayer("Pesho", 15);
 
-            Assert.AreEqual(-1, actualResult);
+            string expected = "Gosho";
+            Assert.AreEqual(2, players.);
         }
-
-        [TestMethod]
-        public void CompareEqualScore()
-        {
-            Score pesho = new Score(15, "Pesho");
-            Score gosho = new Score(15, "Gosho");
-            int actualResult = pesho.CompareTo(gosho);
-
-            Assert.AreEqual(0, actualResult);
-        }
-
-        [TestMethod]
-        public void Compare18To12Score()
-        {
-            Score pesho = new Score(18, "Pesho");
-            Score gosho = new Score(12, "Gosho");
-            int actualResult = pesho.CompareTo(gosho);
-
-            Assert.AreEqual(1, actualResult);
-        }
-
-        [TestMethod]
-        public void TestToStringMethod ()
-        {
-            Score pesho = new Score(18, "Pesho");
-            string peshoToString = pesho.ToString();
-
-            Assert.AreEqual("Pesho with 18 moves.", peshoToString);
-        }
-
-        */
     }
 }
